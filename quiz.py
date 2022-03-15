c1742bd3b81167a7e1f2dd6ab5f7df38d5c42811
Michael Boliekgit config --global user.name Michael <mikebradbol@gmail.com>
2022-03-15 00:51:27 -0400

    Added buttons and the logic of checking whether the right name was clicked

--- quiz.py
+++ quiz.py
@@ -25,8 +25,15 @@ if __name__ == "__main__":
         'Witness': 'witness.jpg', 'Worm': 'worm.jpg', 'Worship': 'worship.jpg'
     }
 
+    def check(button):
+        if correct == buttons[button]:
+            print('correct')
+        else:
+            print('wrong')
+
     # initiate the tkinter window
     window = tk.Tk()
+    window.title('Vow Symbol Quiz')
     
     # randomly choose the four names for the buttons and which of them will be the symbol shown
     buttons = random.choices(symbolNames, k=4)
@@ -35,11 +42,16 @@ if __name__ == "__main__":
     # open the associated file and pack it into a frame
     img = ImageTk.PhotoImage(Image.open(symbolDict[correct]))
     label = tk.Label(window, image=img)
-    label.pack()    
-    print(correct)
+    label.pack()
+    button0 = tk.Button(text=buttons[0], command=lambda n=0: check(n))
+    button0.pack()
+    button1 = tk.Button(text=buttons[1], command=lambda n=1: check(n))
+    button1.pack()
+    button2 = tk.Button(text=buttons[2], command=lambda n=2: check(n))
+    button2.pack()
+    button3 = tk.Button(text=buttons[3], command=lambda n=3: check(n))
+    button3.pack()
     tk.mainloop()
-
-
 
     # determine if correct name was clicked, either loop with a new symbol or end the quiz
 
