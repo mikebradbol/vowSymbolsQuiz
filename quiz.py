ac568468a7259717f4399b0a85dbc6d35af56257
Michael Boliekgit config --global user.name Michael <mikebradbol@gmail.com>
2022-03-15 01:25:52 -0400

    Moved some vars to global, created the game loop, and got rid of the os lib

--- quiz.py
+++ quiz.py
@@ -1,57 +1,76 @@
 # Main program that runs the quiz
 
-import os
 import tkinter as tk
 import random
 from PIL import Image, ImageTk
 
+# global vars
+score = 0
+buttons = []
+correct = ''
+symbolNames = [
+    'Ascendant Realm', 'Commune', 'Darkness', 'Drink', 'Earth', 'Enter', 'Fleet',
+    'Black Garden', 'Give', 'Guardian', 'Black Heart', 'Hive', 'Kill', 'Light', 
+    'Love', 'Pyramid', 'Remember', 'Savathun', 'Scorn', 'Stop', 'Tower', 'Traveler',
+    'Witness', 'Worm', 'Worship'
+]
+
+symbolDict = {
+    'Ascendant Realm': 'ascend.jpg', 'Commune': 'commune.jpg', 'Darkness': 'darkness.jpg', 'Drink': 'drink.jpg',
+    'Earth': 'earth.jpg', 'Enter': 'enter.jpg', 'Fleet': 'fleet.jpg', 'Black Garden': 'garden.jpg', 
+    'Give': 'give.jpg', 'Guardian': 'guardian.jpg', 'Black Heart': 'heart.jpg', 'Hive': 'hive.jpg', 'Kill': 'kill.jpg', 
+    'Light': 'light.jpg', 'Love': 'love.jpg', 'Pyramid': 'pyramid.jpg', 'Remember': 'remember.jpg', 
+    'Savathun': 'savathun.jpg', 'Scorn': 'scorn.jpg', 'Stop': 'stop.jpg', 'Tower': 'tower.jpg', 'Traveler': 'traveler.jpg',
+    'Witness': 'witness.jpg', 'Worm': 'worm.jpg', 'Worship': 'worship.jpg'
+}
+
 if __name__ == "__main__":
-    # initial book keeping and iterables we'll need
-    cwd = os.getcwd()
+    # create seed for random library
     random.seed()
-    symbolNames = [
-        'Ascendant Realm', 'Commune', 'Darkness', 'Drink', 'Earth', 'Enter', 'Fleet',
-        'Black Garden', 'Give', 'Guardian', 'Black Heart', 'Hive', 'Kill', 'Light', 
-        'Love', 'Pyramid', 'Remember', 'Savathun', 'Scorn', 'Stop', 'Tower', 'Traveler',
-        'Witness', 'Worm', 'Worship'
-    ]
-
-    symbolDict = {
-        'Ascendant Realm': 'ascend.jpg', 'Commune': 'commune.jpg', 'Darkness': 'darkness.jpg', 'Drink': 'drink.jpg',
-        'Earth': 'earth.jpg', 'Enter': 'enter.jpg', 'Fleet': 'fleet.jpg', 'Black Garden': 'garden.jpg', 
-        'Give': 'give.jpg', 'Guardian': 'guardian.jpg', 'Black Heart': 'heart.jpg', 'Hive': 'hive.jpg', 'Kill': 'kill.jpg', 
-        'Light': 'light.jpg', 'Love': 'love.jpg', 'Pyramid': 'pyramid.jpg', 'Remember': 'remember.jpg', 
-        'Savathun': 'savathun.jpg', 'Scorn': 'scorn.jpg', 'Stop': 'stop.jpg', 'Tower': 'tower.jpg', 'Traveler': 'traveler.jpg',
-        'Witness': 'witness.jpg', 'Worm': 'worm.jpg', 'Worship': 'worship.jpg'
-    }
-
-    def check(button):
-        if correct == buttons[button]:
-            print('correct')
-        else:
-            print('wrong')
 
     # initiate the tkinter window
     window = tk.Tk()
     window.title('Vow Symbol Quiz')
+
+    # randomly choose 4 symbol names and the image to be shown
+    def getSymbols():
+        global buttons
+        buttons = random.choices(symbolNames, k=4)
+
+    def getCorrect():
+        global correct
+        correct = random.choice(buttons)
+
     
-    # randomly choose the four names for the buttons and which of them will be the symbol shown
-    buttons = random.choices(symbolNames, k=4)
-    correct = random.choice(buttons)
-
-    # open the associated file and pack it into a frame
-    img = ImageTk.PhotoImage(Image.open(symbolDict[correct]))
-    label = tk.Label(window, image=img)
-    label.pack()
-    button0 = tk.Button(text=buttons[0], command=lambda n=0: check(n))
-    button0.pack()
-    button1 = tk.Button(text=buttons[1], command=lambda n=1: check(n))
-    button1.pack()
-    button2 = tk.Button(text=buttons[2], command=lambda n=2: check(n))
-    button2.pack()
-    button3 = tk.Button(text=buttons[3], command=lambda n=3: check(n))
-    button3.pack()
-    tk.mainloop()
-
     # determine if correct name was clicked, either loop with a new symbol or end the quiz
+    def check(button):
+        global score
+        if correct == buttons[button]:
+            print('correct')
+        else:
+            print('wrong')
+    
+    # build the image for the main game screen
+    def gameLoop():
+        getSymbols()
+        getCorrect()
+        img = ImageTk.PhotoImage(Image.open(symbolDict[correct]))
+        print(symbolDict[correct])
+        label = tk.Label(window, image=img)
+        label.pack()
+        button0 = tk.Button(text=buttons[0], command=lambda n=0: check(n))
+        button0.pack()
+        button1 = tk.Button(text=buttons[1], command=lambda n=1: check(n))
+        button1.pack()
+        button2 = tk.Button(text=buttons[2], command=lambda n=2: check(n))
+        button2.pack()
+        button3 = tk.Button(text=buttons[3], command=lambda n=3: check(n))
+        button3.pack()
+        tk.mainloop()
+
+    # begin the game
+    gameLoop()
+    
+
+    
 
